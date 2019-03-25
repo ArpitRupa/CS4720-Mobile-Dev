0d64821ef92483cd0f5fddd45efcb56117243133
Arpit Rupakhetee <ar9cd@virginia.edu>
2019-03-25 15:40:24 -0400

    fixed styling and added env variables

--- MobileProject/firebase.js
+++ MobileProject/firebase.js
@@ -1,7 +1,15 @@
 // In /utils/firebase.js
 // We should import firebase from this module instead of the default package.
 import * as firebase from 'firebase'  // Should not be used elsewhere in the project
-
-firebase.initializeApp(Expo.Constants.manifest.extra.firebase);
+import { senderID, fireKey, fireDomain, database, project, bucket } from "react-native-dotenv";
+let config = {
+    apiKey: fireKey,
+    authDomain: fireDomain,
+    databaseURL: database,
+    projectId: project,
+    storageBucket: bucket,
+    messagingSenderId: senderID
+};
+firebase.initializeApp(config);
 
 export default firebase;
